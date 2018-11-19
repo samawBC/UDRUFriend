32efc6e0eb07b628b4cf5b2d1656adc66566f2f1
koikoi <[A[Bsamawan@gmail.com>
2018-11-19 16:40:26 +0700

    register finish

--- app/src/main/java/com/example/windows10/udrufriend/RegisterFragment.java
+++ app/src/main/java/com/example/windows10/udrufriend/RegisterFragment.java
@@ -2,10 +2,13 @@ package com.example.windows10.udrufriend;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 
 /**
@@ -13,6 +16,22 @@ import android.view.ViewGroup;
  */
 public class RegisterFragment extends Fragment {
 
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        createToolbar();
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle(R.string.register);
+        ((MainActivity)getActivity()).getActionBar().setSubtitle(R.string.message_have_space);
+
+
+    }
 
     public RegisterFragment() {
         // Required empty public constructor
